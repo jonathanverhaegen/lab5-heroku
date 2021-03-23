bb3bf575067919e55029383e4a98e2aedb408bf3
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-03-23 18:54:13 +0100

    added heroku dingen

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const routerMessages = require("./routers/messages");
 const pug = require("pug");
 const mongoose = require('mongoose');
