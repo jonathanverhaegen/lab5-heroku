2bf5f2f859cfb4042b5155e05eda4146e615c900
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-03-24 12:10:49 +0100

    added database password

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routerMessages = require("./routes/api/v1/messages");
 //const pug = require("pug");
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jonathanverhaegen:<password>@cluster0.ghtml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://jonathanverhaegen:a73po5X%&re@cluster0.ghtml.mongodb.net/lab5?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "pug");
 
